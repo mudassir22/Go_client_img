0268a433b6a51e20bc50d7d594944d1a7ae30278
Mudassir Raza <108223551+mudassir22@users.noreply.github.com>
2023-11-11 23:34:04 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,21 +2,20 @@ function sendData() {
     const data = document.getElementById('dataToSend').value;
 
     fetch('https://192.168.0.103:443', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-      // Disable certificate validation (not recommended for production)
-      agent: new (require('https')).Agent({ rejectUnauthorized: false }),
-})
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+        // Note: You don't need to disable certificate validation in the browser
+    })
     .then(response => response.text())
     .then(result => {
         console.log('Server response:', result);
-        alert('Server response:', result);
+        alert('Server response: ' + result);
     })
     .catch(error => {
         console.error('Error sending data:', error);
-        alert('Server response:', result);
+        alert('Error sending data: ' + error);
     });
 }
